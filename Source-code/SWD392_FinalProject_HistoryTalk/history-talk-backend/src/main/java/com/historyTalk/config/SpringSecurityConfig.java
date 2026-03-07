262f088bb22a6fcdcc09d0e0e32176a23161cfb4
VoHaik <khaiqn2707@gmail.com>
2026-03-07 12:12:30 +0700

    Fix CORS

--- Source-code/SWD392_FinalProject_HistoryTalk/history-talk-backend/src/main/java/com/historyTalk/config/SpringSecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package com.historyTalk.config;
-
-// Dead file - replaced by SecurityConfig.java.
-// @Configuration intentionally omitted to prevent conflicting SecurityFilterChain bean registration.
-public class SpringSecurityConfig {
-}
